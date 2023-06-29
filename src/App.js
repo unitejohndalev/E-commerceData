439155c35b2332ddaf4ad3d7c49bec45175e53a0
John Dale <unitejohndalev@gmail.com>
2023-06-29 18:53:36 +0800

    added json file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+//import components
+import NikeProducts from "./components/NikeProducts";
+import LouisVuittonProducts from "./components/LouisVuittonProducts";
+import HM from "./components/HM";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="scrollbar-hide">
+     
+        <NikeProducts />
+        <LouisVuittonProducts />
+        <HM/>
+   
     </div>
   );
 }
